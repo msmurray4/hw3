2846c8501c7a221e917e58ca73d6eb91790abf26
Max Murray <72002297+msmurray4@users.noreply.github.com>
2021-10-04 19:49:04 -0500

    Update pca.py

--- pca.py
+++ pca.py
@@ -31,7 +31,7 @@ def get_eig_perc(S, perc):
     the_eigval, the_eigvec = eigh(S)
     the_val_mean = np.sum(the_eigval)
     upperbound = perc * the_val_mean
-    the_eigval, the_eigvec = eigh(S, subset_by_value=[upperbound, 9999999])
+    the_eigval, the_eigvec = eigh(S, subset_by_value=[upperbound, 99999999])
     order_of_eigVal = np.argsort(the_eigval)[::-1]
     the_eigval = the_eigval[order_of_eigVal]
     the_eigvec = the_eigvec[:, order_of_eigVal]
@@ -39,7 +39,7 @@ def get_eig_perc(S, perc):
     return the_eigval, the_eigvec
 
 
-def project_image(img, U):
+#def project_image(img, U):
     # TODO: add your code here
 
 
@@ -48,11 +48,11 @@ def project_image(img, U):
     # TODO: add your code here
 
 def main():
-        centered_data = load_and_center_dataset('YaleB_32x32.npy')
-        print(len(centered_data))
-        print( len(centered_data[0]))
-        print( np.average(centered_data))
-        print(" ")
+        #centered_data = load_and_center_dataset('YaleB_32x32.npy')
+        #print(len(centered_data))
+        #print( len(centered_data[0]))
+        #print( np.average(centered_data))
+        #print(" ")
         #x = np.array([[1, 2, 5], [3, 4, 7]])
         #print(np.transpose(x))
         #= > array([[1, 3],
@@ -62,18 +62,46 @@ def main():
         #= > array([[30, 46],
         #           [46, 74]])
         #print(np.dot(np.transpose(x), x))
-        covariance = get_covariance(centered_data)
-        print(len(covariance))
-        print(len(covariance[0]))
-        print(" ")
-        Lambda, U = get_eig(covariance, 2)
-        print(Lambda)
-        print(U)
+        #covariance = get_covariance(centered_data)
+        #print(len(covariance))
+        #print(len(covariance[0]))
+        #print(" ")
+        #Lambda, U = get_eig(covariance, 2)
+        #print(Lambda)
+        #print(U)
         #print(np.diag(eigh(covariance, subset_by_index=[1,1])))
-        al, Y = get_eig_perc(covariance, 0.07)
-        print(al)
-        print(Y)
-        projection = project_image(covariance[0], U)
+        #al, Y = get_eig_perc(covariance, 0.07)
+        #print(al)
+        #print(Y)
+        x = load_and_center_dataset('YaleB_32x32.npy')
+        S = get_covariance(x)
+        Lambda, U = get_eig(S, 2)
+        #print(x[0][0]*U[0][0])
+        #print(x[0])
+        print(len(x[0]))
+        #print( U[0])
+        print( len(U))
+        #print(x[0]* U)
+        #temp = []
+        #j = 0
+        #for j in range(len(x) - 1):
+        #    temp[j] = np.dot(x[0][j], U[j])
+
+        #print(x[0][0] * U[0][0] + x[0][0] * U[0][1])
+        sum = 0
+        z = np.transpose(U)
+        for j in range(1023):
+            sum+= x[0][0] * z[0][j] + x[0][0] * z[1][j]
+        print( sum)
+        print(U[0])
+        print(np.transpose(U)[0])
+        print(len(np.transpose(U)[0]))
+        #print ( len ( np.transpose(x[0])))
+        #print(U)
+        #print(np.dot(x[0], np.transpose(U[0])))
+        #projection = project_image(x[0], U)
+        #print(projection)
+        #display_image(x[0], projection)
 
 if __name__ == "__main__":
     main()
